c86dcc83a43a6916fd0947af4c79e693ecb56d5c
燕红立 <1017935195@qq.com>
2021-03-29 11:05:36 +0800

    设置路由权限

--- src/access.ts
+++ src/access.ts
@@ -1,9 +1,24 @@
 /**
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  * */
+interface routeType {
+  path: string;
+  name: string;
+  icon: string;
+  rules: number[];
+  access: string;
+  component: string;
+}
 export default function access(initialState: { currentUser?: API.CurrentUser | undefined }) {
   const { currentUser } = initialState || {};
   return {
-    canAdmin: currentUser && currentUser.roleType === 9,
+    // canAdmin: currentUser && currentUser.roleType === 9,
+    adminRouteFilter: (route: routeType) => {
+      // console.log(route)
+      if (currentUser?.roleType) {
+        return route.rules?.includes(currentUser.roleType);
+      }
+      return false;
+    },
   };
 }
